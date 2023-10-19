f9d643a02e25cb85ddac5dd99d2b3ac636b05469
Edi Ubaedillah <ubaedillahedi@gmail.com>
2023-10-19 10:57:03 +0700

    setting file nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  // devtools: { enabled: false }
+  app: {
+    head: {
+      title: 'Nuxt App Starter',
+      meta: [
+        {charset: 'utf-8'},
+        {name: 'viewport', content: 'width-device-width, initial-scale-1'},
+        {hid: 'description', name: 'description', content: 'Nuxt app starter kit'}
+      ]
+    }
+  }
 })
